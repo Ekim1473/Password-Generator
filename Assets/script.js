fc67a222597e5b8e0b655b3386e1fc11e509a13c
Ekim1473 <123156695+Ekim1473@users.noreply.github.com>
2023-03-01 12:53:04 +1100

    adding CSS Stylesheet

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,17 @@
 // Assignment Code
-const numbers = (0,1,2,3,4,5,6,7,8,9);
-const symbols = ("!","@","#","%","^","&","*","?","$");
-const charlow = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
-const charup = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-
 const generatePassword = (length, hasNumbers,hasSymbols, hasLowercase, hasUppercase) =>{
   const availableCharacters = [
     ...(hasSymbols ? symbols : []),
     ...(hasNumbers ? numbers : []),
     ...(hasUppercase ? charup : []),
     ...(hasLowercase ? charlow : []),
-
   ];
+
+const numbers = (0,1,2,3,4,5,6,7,8,9);
+const symbols = ("!","@","#","%","^","&","*","?","$");
+const charlow = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+const charup = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+
   console.log(availableCharacters);
 
   let password = "";
