857a5eeaaaf84b5f018743148856d3845106ab78
Ekim1473 <123156695+Ekim1473@users.noreply.github.com>
2023-03-01 14:25:23 +1100

    debugging the JS

--- Assets/script.js
+++ Assets/script.js
@@ -7,12 +7,13 @@ const generatePassword = (length, hasNumbers,hasSymbols, hasLowercase, hasUpperc
     ...(hasLowercase ? charlow : []),
   ];
 
+
 const numbers = (0,1,2,3,4,5,6,7,8,9);
 const symbols = ("!","@","#","%","^","&","*","?","$");
 const charlow = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
 const charup = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 
-  console.log(availableCharacters);
+
 
   let password = "";
 
@@ -24,18 +25,18 @@ const charup = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
   }
   return password;
 
-}
-console.log(generatePassword)
+};
+
 
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword;
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
